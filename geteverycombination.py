15083d778b7f1a2d4f71ecc7017d5123634d2ebf
asxlvm <asyluuumso2@gmail.com>
2022-04-11 19:19:04 +0200

    added 3l/3l repeater, 4l/4l repeater and 5l/5l repeater username combinations

--- geteverycombination.py
+++ geteverycombination.py
@@ -5,13 +5,17 @@ import time
 full = string.ascii_lowercase + string.digits
 strlength = 0
 
-def listByLength(length: int):
+def listByLength(length: int, letterOnly = False):
+    if letterOnly == True:
+        chars = string.ascii_lowercase
+    else:
+        chars = full
     combinations = []
     for i in range(length):
         combinations.extend(
                 [
                     ''.join(x) for x in itertools.permutations(
-                        full, i+1
+                        chars, i+1
                     )
                 ]
         )
@@ -42,17 +46,45 @@ def get_input(prompt: str, answer_type, options: list) -> str | int:
             exit()
     return str(answer)
 
+def charGen(strlength: int, filename: str):
+    usernames = listByLength(strlength)
+    with open(filename, "w") as f:
+        f.write(" ".join(usernames))
+        f.write("\n")
+    return usernames
+
+def repeater(strlength: int, filename: str):
+    usernames = []
+    for i in string.ascii_lowercase:
+        usernames.append(i*strlength)
+    with open(filename, "w") as f:
+        f.write(" ".join(usernames))
+        f.write("\n")
+    return usernames
+
+def letterOnly(strlength: int, filename: str):
+    usernames = listByLength(strlength, True)
+    with open(filename, "w") as f:
+        f.write(" ".join(usernames))
+        f.write("\n")
+    return usernames
+
 def wordlistMenu():
     global strlength
+    _type = [["Repeating", repeater], ["Letters only", letterOnly], ["Letters + digits", charGen]]
+    i = 0
+    for option in _type:
+        print((" " * 8), ("×" * len(option[0])), (" " * 2), "\n")
+        print("[ ", str(i), " - ", option[0], " ]")
+        i += 1
+    print("\n")
+    chosentype = int(get_input("What\'s the type of the usernames?", int, []))
     strlength = int(get_input("How many letters should the usernames have?", int, []))
     filename = get_input("Where should I save the usernames?", str, [])
     print(("×" * len("Okay, starting!")), "\n")
     print("Okay, starting!")
     atStart = time.time()
-    usernames = listByLength(strlength)
-    with open(filename, "w") as f:
-        f.write(" ".join(usernames))
-        f.write("\n")
+    usernames = _type[chosentype][1](strlength, filename)
     delta = time.time() - atStart
     print(("×" * len(f"Done! Generated {len(usernames)} username combinations in {delta} seconds!")), "\n")
     print(f"Done! Generated {len(usernames)} username combinations in {delta} seconds!")
